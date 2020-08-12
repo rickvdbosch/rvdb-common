3cdad029197594a3f57272895e24362388f62572
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-12 14:50:36 +0200

    Added SettingsHelper Added TableStorageHelper Added comments to helper methods

---
+++ RvdB.Common.Azure/Helpers/SettingsHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RvdB.Common.Azure.Helpers
+{
+    public static class SettingsHelper
+    {
+        /// <summary>
+        /// Gets a settign from Environment Variables.
+        /// </summary>
+        /// <param name="name">The name of the setting to get.</param>
+        /// <returns>The value of the setting with the specified <paramref name="name"/>.</returns>
+        public static string GetEnvironmentVariable(string name)
+        {
+            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+        }
+    }
+}
---
+++ RvdB.Common.Azure/Helpers/TableStorageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Cosmos.Table;
+
+namespace RvdB.Common.Azure.Helpers
+{
+    public static class TableStorageHelper
+    {
+        /// <summary>
+        /// Gets all entities from the provided <see cref="CloudTable"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of entity to get from <paramref name="table"/>.</typeparam>
+        /// <param name="table"><see cref="CloudTable"/> to get all entities from.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing all entities from <paramref name="table"/>.</returns>
+        public static async Task<IEnumerable<T>> GetEntitiesAsync<T>(CloudTable table) where T : ITableEntity, new()
+        {
+            var entities = new List<T>();
+            var query = new TableQuery<T>();
+            TableQuerySegment<T> querySegment = null;
+
+            do
+            {
+                querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+                entities.AddRange(querySegment.Results);
+            } while (querySegment.ContinuationToken != null);
+
+            return entities;
+        }
+    }
+}
