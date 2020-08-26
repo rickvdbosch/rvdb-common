d0fea7dfb7a408e6e173252e279bd1e00421e830
Rick van den Bosch <rickvdbosch@outlook.com>
2020-08-26 11:14:10 +0200

    Implemented GetEntitiesFromPartitionAsync Updated Nuget packages

--- RvdB.Common.Azure/Helpers/TableStorageHelper.cs
+++ RvdB.Common.Azure/Helpers/TableStorageHelper.cs
@@ -27,5 +27,26 @@ namespace RvdB.Common.Azure.Helpers
 
             return entities;
         }
+
+        /// <summary>
+        /// Gets all entities in the provided <paramref name="partitionKey"/> from the provided <see cref="CloudTable"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of entity to get from <paramref name="table"/>.</typeparam>
+        /// <param name="table"><see cref="CloudTable"/> to get the entities from.</param>
+        /// <returns>An <see cref="IEnumerable{T}"/> containing all entities from the <paramref name="partitionKey"/> partition in the <paramref name="table"/>.</returns>
+        public static async Task<IEnumerable<T>> GetEntitiesFromPartitionAsync<T>(CloudTable table, string partitionKey) where T : ITableEntity, new()
+        {
+            var entities = new List<T>();
+            TableQuerySegment<T> querySegment = null;
+            var query = new TableQuery<T>().Where(TableQuery.GenerateFilterCondition(nameof(ITableEntity.PartitionKey), QueryComparisons.Equal, partitionKey));
+
+            do
+            {
+                querySegment = await table.ExecuteQuerySegmentedAsync(query, querySegment?.ContinuationToken);
+                entities.AddRange(querySegment.Results);
+            } while (querySegment.ContinuationToken != null);
+
+            return entities;
+        }
     }
 }
