1ac5d819f0590ddf2ca60b257a15ec40349842af
Rick van den Bosch <rickvdbosch@outlook.com>
2021-03-22 09:01:08 +0100

    Setting!

--- RvdB.Common.Azure/Helpers/SettingsHelper.cs
+++ RvdB.Common.Azure/Helpers/SettingsHelper.cs
@@ -5,13 +5,13 @@ namespace RvdB.Common.Azure.Helpers
     public static class SettingsHelper
     {
         /// <summary>
-        /// Gets a settign from Environment Variables.
+        /// Gets a variable from Environment Variables.
         /// </summary>
-        /// <param name="name">The name of the setting to get.</param>
-        /// <returns>The value of the setting with the specified <paramref name="name"/>.</returns>
-        public static string GetEnvironmentVariable(string name)
+        /// <param name="variable">The name of the variable to get.</param>
+        /// <returns>The value of the specified <paramref name="variable"/>.</returns>
+        public static string GetEnvironmentVariable(string variable)
         {
-            return Environment.GetEnvironmentVariable(name, EnvironmentVariableTarget.Process);
+            return Environment.GetEnvironmentVariable(variable, EnvironmentVariableTarget.Process);
         }
     }
 }
