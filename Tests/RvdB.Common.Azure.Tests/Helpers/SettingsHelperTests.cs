bdd76f5a1d498ab7e39f5480bc61afec17c96b03
Rick van den Bosch <rickvdbosch@outlook.com>
2021-03-22 09:05:05 +0100

    Added SettingsHelperTests

---
+++ Tests/RvdB.Common.Azure.Tests/Helpers/SettingsHelperTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using RvdB.Common.Azure.Helpers;
+
+using Xunit;
+
+namespace RvdB.Common.Azure.Tests.Helpers
+{
+	public class SettingsHelperTests
+	{
+		#region Constants
+
+		private const string VALID_NAME = "validName";
+		private const string INVALID_NAME = "invalidName";
+		private const string EXPECTED_VALUE = "testValue";
+
+		#endregion
+
+		[Fact]
+		public void GettingEnvVariable_WithNameIsNull_ThrowsArgumentNullException()
+		{
+			var ex = Assert.Throws<ArgumentNullException>(() => SettingsHelper.GetEnvironmentVariable(null));
+			Assert.Equal("variable", ex.ParamName);
+		}
+
+		[Fact]
+		public void GettingEnvVariable_WithValidName_ReturnsValue()
+		{
+			Environment.SetEnvironmentVariable(VALID_NAME, EXPECTED_VALUE);
+			var variable = SettingsHelper.GetEnvironmentVariable(VALID_NAME);
+			Assert.Equal(EXPECTED_VALUE, variable);
+			Environment.SetEnvironmentVariable(VALID_NAME, null);
+		}
+
+		[Fact]
+		public void GettingEnvVariable_WithInvalidName_ReturnsNull()
+		{
+			var variable = SettingsHelper.GetEnvironmentVariable(INVALID_NAME);
+			Assert.Null(variable);
+		}
+	}
+}
